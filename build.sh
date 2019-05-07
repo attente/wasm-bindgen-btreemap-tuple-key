0ac6f88e99e82c246789c78c3f5eeb21abcfab70
William Hua <william@attente.ca>
2019-05-07 16:55:24 -0400

    wasm-bindgen --nodejs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cargo build --release --target=wasm32-unknown-unknown
-wasm-bindgen --out-dir src --out-name bindings target/wasm32-unknown-unknown/release/wasm_bindgen_btreemap_tuple_key.wasm
+wasm-bindgen --nodejs --out-dir src --out-name bindings target/wasm32-unknown-unknown/release/wasm_bindgen_btreemap_tuple_key.wasm
 yarn tsc
